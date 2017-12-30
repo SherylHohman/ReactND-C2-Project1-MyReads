0aa13cad8bbc31fcd3ad774138767b3aad0750a6
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 01:51:53 -0800

    feat: wip test: call <Book/> in App with dummy prop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import BookshelfChanger from './BookshelfChanger';
+import Book from './Book';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -61,6 +62,7 @@ class BooksApp extends React.Component {
                   <div className="bookshelf-books">
                     <ol className="books-grid">
                       <li>
+                      {/*}
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -71,8 +73,11 @@ class BooksApp extends React.Component {
                           <div className="book-title">To Kill a Mockingbird</div>
                           <div className="book-authors">Harper Lee</div>
                         </div>
+                      */}
+                      <Book book={this.state.books[0]} />
                       </li>
                       <li>
+                      {/*}  */} 
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
@@ -83,6 +88,9 @@ class BooksApp extends React.Component {
                           <div className="book-title">Ender's Game</div>
                           <div className="book-authors">Orson Scott Card</div>
                         </div>
+                      {/* */}
+
+
                       </li>
                     </ol>
                   </div>
