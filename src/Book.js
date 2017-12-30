6db6fec436028a84031360e2e496dec6933ab262
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 01:15:01 -0800

    feat: create Book skeleton component
    TODO: remove hard-coded book, replace with props for dynamic rendering

---
+++ src/Book.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Book = function(props){
+  return (
+
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+
+        <BookshelfChanger />
+
+      </div>
+      <div className="book-title">To Kill a Mockingbird</div>
+      <div className="book-authors">Harper Lee</div>
+    </div>
+
+  )
+}
+
+export default Book;
