20c50876ab6d0eee16d60f269097a457fcda4f76
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 15:09:48 -0800

    feat: map over books array to dynamically show all
    With this I removed the hard coded book reference.
      Now books is mapped over, showing all books on all Bookshelves
    TODO: filter books by bookshelf

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -11,25 +11,27 @@ const Bookshelf = function(props){
       <h2 className="bookshelf-title">{shelfTitle}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
-          <li>
 
             {/*
-              Delay rendering of <Book/> until state.books has data (from Ajax request)
-                Why: Can't access 0th index of an empty array !
+              Delay rendering of <Book/> until books has data (from Ajax request)
+                B/C: Can't access 0th index of an empty array !
                 TODO: render a placeholder book instead
-              TODO: replace hard-coded array index
             */}
-            {books.length > 0 &&
-              <Book book={books[0]}
-            />}
-
-          </li>
+            {books.length > 0 && (
+              books.map((book) => (
+                <li key={book.id}><Book book={book}/></li>
+              ))
+            )}
 
         </ol>
       </div>
     </div>
   );
 };
+
+// TODO: can use this for placeholder book "image" while waiting for fetched data
+//      <Book book={books.map(() => {})}
+//      aaccompanied by "Loading Books" text
 
 Bookshelf.propTypes = {
   books: PropTypes.array.isRequired,
