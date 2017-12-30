0d0d54cb6869f36eccbf56fa019d816b92a5acb0
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 05:44:07 -0800

    feat: refactor Book component uses props.book data

--- src/Book.js
+++ src/Book.js
@@ -3,20 +3,27 @@ import BookshelfChanger from './BookshelfChanger';
 import PropTypes from 'prop-types'
 
 const Book = function(props){
-  return (
 
+  // convenience variables
+  const {title, authors, bookCoverURL} = props.book;
+
+  return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-
+        <div
+            className="book-cover"
+            style={{ width: 128, height: 193,
+              backgroundImage: `url(${bookCoverURL})`
+            }}
+        >
+        </div>
         <BookshelfChanger />
-
       </div>
-      <div className="book-title">To Kill a Mockingbird</div>
-      <div className="book-authors">Harper Lee</div>
+      <div className="book-title">{title}</div>
+      <div className="book-authors">{authors}</div>
     </div>
-
   )
+
 }
 
 Book.propTypes = {
