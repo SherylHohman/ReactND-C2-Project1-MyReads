3c716f1e5bf250977850c4d21299bc01edcbf65e
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 19:52:28 -0800

    style: change "Loading message", whitespace, comments

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -7,29 +7,27 @@ const Bookshelf = function(props){
   const { books, shelfTitle, shelf } = props;
 
   return (
+
     <div className="bookshelf">
       <h2 className="bookshelf-title">{shelfTitle}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
 
-          {/* mapping over an empty array, simply returns an empty array
-              So, we can never try accessing an index that doesn't exist
-              Therefore, we needn't wait until Ajax request returns us data
-              Lack of books can no longer break our app.
-              Conditional JSX rendering can thus be removed!
-          */}
+          {/* show books */}
           {books.filter((book) => (book.shelf === shelf))
-                .map((book) => (<li key={book.id}><Book book={book}/></li>
+                .map((book) => (
+                  <li key={book.id}><Book book={book}/></li>
           ))}
 
+          {/* ...or Loading message */}
           {books.length === 0 && (
-            /* TODO: make this prettier :-)*/
-            <p>Get Comfy while we Retrieve your books.. </p>
+            <p>Retrieving your books.. </p>
           )}
 
         </ol>
       </div>
     </div>
+
   );
 };
 
