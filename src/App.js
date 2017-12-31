3df2c20f843dd2c1160b83e8677f1b7287ec5f5b
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-30 17:46:45 -0800

    feat: import BrowserRouter, and Router
    despite the suggestion to not change index.js,
    	I imported Browser Router and added that component to the file.
    	I want address bar URL changes to function as expected.
    	.. and the component needs to surround <App/>.
    	Therefore it had to be added to index.js, afaik
    	I'll change back if I find another way.
    TODO: reafactor App.js to use <Route/>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Route } from 'react-router-dom';
 import ListBooks from './ListBooks';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <BrowserRouter><App /></BrowserRouter>,
+  document.getElementById('root')
+)
+
+// I know the instructions said not to change this file.
+// However I felt it was the best place to add BrowserRouter..
+//  which I felt was needed for my app.
