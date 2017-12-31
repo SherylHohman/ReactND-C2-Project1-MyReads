0796ff87997c8e562b9ff4f1bf0d78b418dab877
SherylHohman <SherylHohman@users.noreply.github.com>
2017-12-31 11:22:10 -0800

    refactor: <BookshelfChanger/> to class Component
    add notes: thinking through how to approach wiring
    	this component up with expected functionality:
    	it should add/move "its" book to the shelf selected by the user
    	ie: change the value of the book's `shelf` value.

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BookshelfChanger = function(props){
-  return (
+class BookshelfChanger extends Component {
 
-    <div className="book-shelf-changer">
-      <select>
-        <option value="none" disabled>Move to...</option>
-        <option value="currentlyReading">Currently Reading</option>
-        <option value="wantToRead">Want to Read</option>
-        <option value="haveRead">Have Read</option>
-        <option value="none">None</option>
-      </select>
-    </div>
+render() {
+    return (
 
-  );
+      <div className="book-shelf-changer">
+        <select>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="haveRead">Have Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+
+    );
+  }
+
 }
 
 export default BookshelfChanger;
+
+/*
+  Thinking of how to approach this:
+  Turn <select> into a Controlled Component.
+    Then, the "selected option" will be controlled by
+      this.state.shelf
+    and "Event" handler will then be bound to an "onHandleEvent"
+      method on <BooksApp/> that
+      - changes This Book's shelf property's value (this.books[myBook].shelf)
+      - which causes a trickle down re-render, updating the
+        option selected in this BookshelfChanger component
+    Then, how do I pass that value around?  This BookshelfChanger
+      component must recieve "thisBook/myBook/book" as a PROP
+      from it's owner.
+    No "Submit" button is needed, because select's `onChange` property
+      will be used, dynamically and automatically firing every time
+      the user changes their "selected/chosen" value.
+
+  What I'm aiming to accomplish:
+  wire this component up with user-expected functionality:
+    - it should add/move "its" book to the shelf selected by the user
+    - ie: change the value of the book's `shelf` value.
+
+*/
